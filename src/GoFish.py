one small bug fix for the derivative w.r.t. beta phi (only matters for non-LCDM case)

src/GoFish.py
@@ -43,7 +43,6 @@ if __name__ == "__main__":
         cosmo.volume = data.volume * 1.0e9
 
     console.log("computed CAMB linear matter power spectra for redshifts.")
-
     # Convert the nz to nbar in (h/Mpc)^3
     if data.nbar is None:  # we got a file with nz instead of nbar, so do a conversion
         data.convert_nbar(cosmo.volume, float(pardict["skyarea"]))
@@ -207,7 +206,7 @@ if __name__ == "__main__":
             # cov = dgesv(Catch, identity)[2]
             Catch_small = Catch.copy()
             Catch_small = shrink_sqr_matrix(Catch_small)
-            Catch_small[abs(Catch_small) < 1.0e-25] = (
+            Catch_small[abs(Catch_small) < 1.0e-20] = (
                 0.0  # remove small values making the inversion very unstable
             )
             cov = np.linalg.inv(Catch_small)
@@ -322,14 +321,14 @@ if __name__ == "__main__":
     FullCatchsmall = FullCatch.copy()
     for fi in np.arange(len(data.nz)):
         for fj in np.arange(len(data.nz[0])):
-            if data.nz[fi][fj] <= 1.0e-10:
+            if data.nz[fi][fj] <= 1.0e-20:
                 flags.append(fj * len(data.nz) + fi)
     flags = np.sort(np.array(flags))
 
     if len(flags) > 0:
         console.log("Removing rows for zero number density")
         FullCatchsmall = shrink_sqr_matrix(FullCatch, flags)
-        FullCatchsmall[abs(FullCatchsmall) < 1.0e-25] = (
+        FullCatchsmall[abs(FullCatchsmall) < 1.0e-20] = (
             0.0  # remove small values making the inversion very unstable
         )
 
@@ -338,7 +337,7 @@ if __name__ == "__main__":
         console.log(
             "Checking if fisher information is zero for galaxy bias in any rows (must remove these)?"
         )
-        flags = np.where(abs(np.diag(FullCatchsmall)) <= 1.0e-25)[0]
+        flags = np.where(abs(np.diag(FullCatchsmall)) <= 1.0e-20)[0]
         print(flags)
         if len(flags) > 0:
             if (
@@ -453,6 +452,13 @@ if __name__ == "__main__":
     # print(np.sqrt(np.diag(np.linalg.inv(FullCatchsmall[-3:, -3:]))))
     # print(np.sqrt(np.diag(np.linalg.inv(FullCatchsmall[-1:, -1:]))))
     # np.savetxt('Fisher_matrix.txt', FullCatch)
+
+    if not pardict.as_bool("beta_phi_fixed") and not pardict.as_bool("geff_fixed"):
+        print("Correlation coefficient beta and log10Geff:")
+        print(
+            cov_renormFull[-1, -2]
+            / (np.sqrt(cov_renormFull[-1, -1]) * np.sqrt(cov_renormFull[-2, -2]))
+        )
 
     cov_main = None
     if pardict["do_combined_DESI"] == "True":

src/TackleBox.py
@@ -3,7 +3,12 @@ from findiff import FinDiff
 from scipy.integrate import simpson as simps
 from scipy.interpolate import splrep, splev
 from loguru import logger
-from ioutils import CosmoResults, InputData, fitting_formula_Baumann19, derivk_geff
+from ioutils import (
+    CosmoResults,
+    InputData,
+    derivk_geff,
+    fitting_formula_interactingneutrinos,
+)
 import numpy.typing as npt
 
 
@@ -159,7 +164,12 @@ def compute_deriv_betaphiamplitude(cosmo: CosmoResults):
         )
 
     derPk = FinDiff(0, dk, acc=4)(pkarray)[order]
-    dk_dbeta = fitting_formula_Baumann19(cosmo.k) / cosmo.r_d
+    # dk_dbeta = fitting_formula_Baumann19(cosmo.k) / cosmo.r_d
+    dk_dbeta = (
+        fitting_formula_interactingneutrinos(cosmo.k, cosmo.log10Geff, cosmo.r_d)
+        / cosmo.r_d
+    )
+
     derPbeta_amplitude = np.outer(
         derPk * dk_dbeta, np.ones(len(mu))
     )  # dP(k')/dbeta = dP/dk' * dk'/dbeta , dk'/dbeta = f(k')/r_s

src/ioutils.py
@@ -16,11 +16,13 @@ class InputData:
             df["volume"].to_numpy() if "volume" in df.keys() else -np.ones(len(df))
         )
 
-        self.nbar = (
-            np.array([df[i] for i in df.keys() if "nbar" in i]) * 1e-3
-            if "nbar" in df.keys()
-            else None
-        )
+        self.nbar = np.array([df[i] for i in df.keys() if "nbar" in i]) * 1e-3
+        if len(self.nbar) == 0:
+            self.nbar = None
+        else:
+            self.nbar[self.nbar == 0] = (
+                1.0e-40  # Set any zero number densities to a small number
+            )
 
         # Sort out any tracers without galaxies in a particular redshift bin
         self.remove_null_tracers()
@@ -191,7 +193,7 @@ class CosmoResults:
         pars.InitPower.set_params(
             As=float(parlinear["A_s"]), ns=float(parlinear["n_s"])
         )
-        pars.set_matter_power(redshifts=np.concatenate([zmid[::-1], [0.0]]), kmax=5.0)
+        pars.set_matter_power(redshifts=np.concatenate([zmid[::-1], [0.0]]), kmax=10.0)
         pars.set_cosmology(
             H0=parlinear["H0"],
             omch2=float(parlinear["omega_cdm"]),
@@ -210,7 +212,7 @@ class CosmoResults:
 
         # Get the power spectrum
         kin, zin, pklin = results.get_matter_power_spectrum(
-            minkh=2.0e-5, maxkh=5.0, npoints=2000
+            minkh=2.0e-5, maxkh=10.0, npoints=2000
         )
 
         # Get some derived quantities
@@ -225,17 +227,19 @@ class CosmoResults:
         r_d = results.get_derived_params()["rdrag"]
         f = fsigma8 / sigma8
         growth = sigma8 / results.get_sigma8()[-1]
-        alpha_nu = (8.0 / 7.0) * (11.0 / 4.0) ** (4.0 / 3.0)
-        beta_phi = (parlinear["Neff"] / (alpha_nu + parlinear["Neff"])) / (
-            3.044 / (alpha_nu + 3.044)
-        )
+        # alpha_nu = (8.0 / 7.0) * (11.0 / 4.0) ** (4.0 / 3.0)
+        beta_phi = parlinear["beta_phi"] if "beta_phi" in parlinear.keys() else 1.0
         log10Geff = (
             pardict.as_float("log10Geff") if "log10Geff" in pardict.keys() else -np.inf
         )
 
-        pk_splines = [splrep(kin, pklin[i + 1]) for i in range(len(zin[1:]))]
+        kshift = (
+            fitting_formula_interactingneutrinos(kin, log10Geff, r_d)
+            - fitting_formula_Baumann19(kin)
+        ) / r_d
+        pk_splines = [splrep((kin + kshift), pklin[i + 1]) for i in range(len(zin[1:]))]
         pksmooth_splines = [
-            splrep(kin, self.smooth_hinton2017(kin, pklin[i + 1]))
+            splrep(kin + kshift, self.smooth_hinton2017(kin, pklin[i + 1]))
             for i in range(len(zin[1:]))
         ]
 
@@ -284,7 +288,7 @@ class CosmoResults:
         ks: npt.NDArray,
         pk: npt.NDArray,
         degree: float = 13,
-        sigma: float = 1,
+        sigma: float = 0.5,
         weight: float = 0.5,
     ):
         """Smooth power spectrum based on Hinton et. al., 2017 polynomial method
@@ -396,7 +400,7 @@ def amplitude_modulation_geff(
         + -3.04729338e-01 * (log10Geff)
         + 5.89273173e-01
     )
-    if log10Geff < -12:
+    if log10Geff < -6:
         amplitude_modulation = 1.0
     return amplitude_modulation
 
@@ -418,7 +422,7 @@ def exponential_damping_geff(
     exponential_damp_modulation = (
         exponential_damp_modulation + 1.09893067e-02 * (log10Geff) + -2.89929198e-02
     )
-    if log10Geff < -12:
+    if log10Geff < -6:
         exponential_damp_modulation = 0.0
     exponential_damping = np.exp(ks * rs * exponential_damp_modulation)
     return exponential_damping
@@ -431,11 +435,10 @@ def fitting_formula_interactingneutrinos(
     and multiply by new parameters to capture impact of log10Geff on the phase shift."""
     standard_phase = fitting_formula_Baumann19(ks)
     amplitude_modulation = amplitude_modulation_geff(ks, log10Geff, rs)
-    if log10Geff < -12:
+    if log10Geff < -6:
         amplitude_modulation = 1.0
     exponential_damp_modulation = exponential_damping_geff(ks, log10Geff, rs)
-    exponential_damping = np.exp(ks * rs * exponential_damp_modulation)
-    return amplitude_modulation * standard_phase * exponential_damping
+    return amplitude_modulation * standard_phase * exponential_damp_modulation
 
 
 def deriv_amplitude_modulation_geff(
@@ -453,7 +456,7 @@ def deriv_amplitude_modulation_geff(
     amplitude_modulation_der = (
         amplitude_modulation_der + 2 * -5.52743545e-02 * (log10Geff) + -3.04729338e-01
     )
-    if log10Geff < -12:
+    if log10Geff < -6:
         amplitude_modulation_der = 0.0
     return amplitude_modulation_der
 
@@ -473,7 +476,7 @@ def deriv_exponential_damping_geff(
         + 2 * 2.80788885e-02 * (log10Geff)
     )
     exponential_damp_modulation_der = exponential_damp_modulation_der + 1.09893067e-02
-    if log10Geff < -12:
+    if log10Geff < -6:
         exponential_damp_modulation_der = 0.0
     return exponential_damp_modulation_der
 